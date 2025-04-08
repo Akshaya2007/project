870a19532fbc3f396e843139647c5fa7555e8c4f
Akshaya2007 <81800055+Akshaya2007@users.noreply.github.com>
2025-04-08 07:58:58 +0545

    Update landjs.js

--- landjs.js
+++ landjs.js
@@ -1,6 +1,18 @@
-const toggle = document.getElementById('darkToggle');
-const body = document.body;
+const toggle = document.getElementById('toggle');
+const root = document.documentElement;
 
 toggle.addEventListener('change', () => {
-  body.classList.toggle('dark-mode');
+  if (toggle.checked) {
+    root.style.setProperty('--white', '#111');
+    root.style.setProperty('--text', '#fff');
+    root.style.setProperty('--cyan', '#008b8b');
+    root.style.setProperty('--pink', '#c71585');
+    root.style.setProperty('--gold', '#b8860b');
+  } else {
+    root.style.setProperty('--white', '#ffffff');
+    root.style.setProperty('--text', '#000000');
+    root.style.setProperty('--cyan', '#00d4ff');
+    root.style.setProperty('--pink', '#ff9ee6');
+    root.style.setProperty('--gold', '#ffd700');
+  }
 });
